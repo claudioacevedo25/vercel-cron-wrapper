f93ee68e9661e28ff715a5434ed5a8b02bcdf669
Maxi <claudioacevedo25@gmail.com>
2025-06-23 09:08:33 +0200

    chore: basic home page

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="ph-vercel-cron-wrapper">
+      <main className="ph-vercel-cron-wrapper__main">
+        <h1 className="ph-vercel-cron-wrapper__title">Vercel Cron Wrapper</h1>
+      </main>
+    </div>
+  )
+}
